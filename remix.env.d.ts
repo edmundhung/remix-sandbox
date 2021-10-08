ef10a6f3f213b21ac4a92d45afd218971037bc1e
edmundhung <edmundhung@users.noreply.github.com>
2021-10-08 01:14:03 +0000

    chore: publish template for version 0.19.0-pre.1

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,3 +1,3 @@
-// Copyright © 2021 React Training LLC. All rights reserved.
+// Copyright © 2021 Remix Software Inc. All rights reserved.
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
